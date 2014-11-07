d2b8f4dcde57aed0cc8839fcc95b05ea22c51246
nixx <nixx.dj@gmail.com>
2014-11-07 12:21:30 +0300

    + add config to packer

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	'package' => 'rees46',
+	'destination' => '.',
+	'directories' => array(
+		'./classes/modules/rees46',
+		'./styles/skins/mac/data/modules/rees46',
+		'./templates/demodizzy/xslt/modules/rees46',
+		'./templates/demoold/tpls/rees46',
+	),
+	'files' => array(
+		'./images/cms/admin/mac/icons/big/rees46.png',
+		'./images/cms/admin/mac/icons/medium/rees46.png',
+		'./images/cms/admin/mac/icons/small/rees46.png',
+		'./js/rees46.js',
+	),
+);
